36301e0a2824c5c1242ef43db0a708bee0580841
Richard Gibson <richard.gibson@gmail.com>
2019-03-28 00:37:21 -0400

    Improve test cases page

--- docs/cases.js
+++ docs/cases.js
@@ -9,39 +9,38 @@ function onload() {
 	var refs = document.querySelectorAll(".cases tbody td[data-ref]");
 	Array.prototype.forEach.call(refs, function( elRef ) {
 		var elLink = document.createElement("a");
-	  elLink.setAttribute("href", elLink.getAttribute("data-ref"));
-	  elLink.setAttribute("target", "_blank");
-	  elLink.innerHTML = elRef.innerHTML + "→";
-	  elRef.innerHTML = " ";
+		elLink.setAttribute("href", elRef.getAttribute("data-ref"));
+		elLink.setAttribute("target", "_blank");
+		elLink.appendChild(document.createTextNode("→"));
+		elRef.appendChild(document.createTextNode(" "));
 		elRef.appendChild(elLink);
 	});
 
 	var cases = document.querySelectorAll(".cases tbody tr:not(.group-head)");
 	Array.prototype.forEach.call(cases, function( elCase ) {
-	  // Collect input.
-	  var elInput = elCase.querySelector("td");
-	  var strInput = elInput.innerText;
-	  
-	  // Attempt parsing.
-	  var tsOutput = Date.parse(strInput);
-	  var bRejected = isNaN(tsOutput);
-	  
-	  // Output results.
-	  var elOutput = elCase.appendChild(document.createElement("td"));
-	  if ( bRejected ) {
-		elOutput.innerHTML = "<code>NaN</code>";
-	  }
-	  else {
-		// Try to match the input format w.r.t. time.
-		var strOutput = new Date(tsOutput).toISOString();
-		if ( /[T\x20]./i.test(strInput) ) {
-		  var precision = strInput.match(/[:.,]/g).length;
-		  var rTime = new RegExp("(([:.]\\d+){" + precision + "})[^Z]*");
-		  strOutput = strOutput.replace(rTime, "$1");
+		// Collect input.
+		var elInput = elCase.querySelector("td");
+		var strInput = elInput.innerText;
+
+		// Attempt parsing.
+		var tsOutput = Date.parse(strInput);
+		var bRejected = isNaN(tsOutput);
+
+		// Output results.
+		var elOutput = elCase.appendChild(document.createElement("td"));
+		if ( bRejected ) {
+			elOutput.innerHTML = "<code>NaN</code>";
 		} else {
-			strOutput = strOutput.replace(/:\d+\.\d+/i, "");
+			// Try to match the input format w.r.t. time.
+			var strOutput = new Date(tsOutput).toISOString();
+			if ( /[T\x20]./i.test(strInput) ) {
+				var precision = strInput.match(/[:.,]/g).length;
+				var rTime = new RegExp("(([:.]\\d+){" + precision + "})[^Z]*");
+				strOutput = strOutput.replace(rTime, "$1");
+			} else {
+				strOutput = strOutput.replace(/:\d+\.\d+/i, "");
+			}
+			elOutput.innerText = strOutput;
 		}
-		elOutput.innerText = strOutput;
-	  }
 	});
 }
